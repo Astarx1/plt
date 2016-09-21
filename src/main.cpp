f839bc213d694c9f47998c47fef781a893201a13
POLEYA <john.poleya@ensea.fr>
2016-09-21 10:37:10 +0200

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-// Les lignes suivantes ne servent qu'à vérifier que la compilation avec SFML fonctionne
+//test Les lignes suivantes ne servent qu'à vérifier que la compilation avec SFML fonctionne
 #include <SFML/Graphics.hpp>
 
 void testSFML() {
