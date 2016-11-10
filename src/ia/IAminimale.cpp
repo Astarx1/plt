931a1ea8b0f27e6ae7fcc6a090e240dbef236895
POLEYA <john.poleya@ensea.fr>
2016-11-10 19:03:02 +0100

    commit ia

--- src/ia/IAminimale.cpp
+++ src/ia/IAminimale.cpp
@@ -1,3 +1,4 @@
+#include <vector>
 #include "../ia.h"
 #include "../engine.h"
 #include "../state.h"
@@ -18,11 +19,16 @@ Commande IAminimale::exec_cmd(int id, sf::Time time){
     Personnage& perso = this->etat->getRefPersonnage(id);
     
     std::vector<int> params;
+    params.resize(2);
     if(!perso.getEnDeplacement()){
         params[0]= rand()%20 +1;
         params[1]= rand()%12 +1;
     }
-    
+    else{
+        params[0]= perso.getXobj();
+        params[1]= perso.getYobj();
+        
+    }
     Commande c(etat, "d" ,time,params,id);
     return c;
 }
