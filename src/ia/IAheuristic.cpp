c7da36a732b88d3904d8fd13683b7b7ed74168d3
POLEYA <john.poleya@ensea.fr>
2016-11-16 09:06:56 +0100

    commit IAheuristic + dia modif

--- src/ia/IAheuristic.cpp
+++ src/ia/IAheuristic.cpp
@@ -7,15 +7,25 @@ using namespace ia;
 using namespace engine;
 using namespace std;
 
-Personnage* IAheuristic::cible(std::vector<state::Personnage*> ennemis){
+Personnage* IAheuristic::cible(std::vector<state::Personnage*> ennemis, int id, state::Etat* etat){
     
     Personnage* perso = ennemis[0];
     std::vector< state::Personnage* > cibles;
+    std::vector<int> pos_ennemi, pos_joueur, pos_cible;
+    pos_ennemi.resize(2);
+    pos_joueur.resize(2);
+    pos_cible.resize(2);
+    int dmin = 0, index = 0;
+    
+    pos_joueur[0] = etat->getRefPersonnage(id).getX();
+    pos_joueur[1] = etat->getRefPersonnage(id).getY();
     
     cibles.push_back(perso);
     
+    
     if(ennemis.size() > 1){
-       for(int i=1; i < ennemis.size()-1; i++){
+       // Cible l'ennemi le ayant le moins de PV
+        for(int i=1; i < ennemis.size()-1; i++){
             if(perso->getVie() == ennemis[i]->getVie()){
                 cibles.push_back(ennemis[i]);
             }
@@ -25,13 +35,28 @@ Personnage* IAheuristic::cible(std::vector<state::Personnage*> ennemis){
                 cibles.push_back(perso);
             }
         }
-       /*
+      // Si plusieurs ennemis possèdent le même PV min, alors on cible le plus proche
        if(cibles.size()>1){
-           perso = cibles.pop_back();
+           pos_ennemi[0] = cibles.at(0)->getX();
+           pos_ennemi[1] = cibles.at(0)->getY();
+           dmin = sqrt(pow(pos_ennemi[0]-pos_joueur[0],2)+pow(pos_ennemi[1]-pos_joueur[1],2));
+           
+           for(int i=1; i < cibles.size()-1; i++){
+              pos_ennemi[0] = cibles.at(i)->getX();
+              pos_ennemi[1] = cibles.at(i)->getY();
+              
+              int tmp = sqrt(pow(pos_ennemi[0]-pos_joueur[0],2)+pow(pos_ennemi[1]-pos_joueur[1],2));
+              
+              if(tmp < dmin){
+                  dmin = tmp;
+                  index = i;
+              }
+           }
+           perso = cibles.at(index);
        }
        else{
-           perso = cibles.pop_back();
-       }*/
+           perso = cibles.at(0);
+       }
     }
    
     return perso;  
