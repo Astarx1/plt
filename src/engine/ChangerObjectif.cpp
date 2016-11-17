2bb88de6e6ec5ae9c45787ed29f38b6bd14ca547
Romain Amuat <romain.amuat@gmail.com>
2016-11-17 20:52:42 +0100

    Ajout verification deplacement

--- src/engine/ChangerObjectif.cpp
+++ src/engine/ChangerObjectif.cpp
@@ -11,6 +11,27 @@ using namespace engine;
 void ChangerObjectif::run(Etat* e, std::vector<int> params, sf::Time t) {
 	Personnage & p = e->getRefPersonnage(params[0]);
 	if (params[1] != -1 && params[2] != -1) {
+		if (e->getMapActuel() == 1) {
+			if (params[1] >= 20) 
+				params[1] = 19;
+			if (params[1] <= 0) 
+				params[1] = 1;
+			if (params[2] >= 16) 
+				params[2] = 15;
+			if (params[2] <= 5) 
+				params[1] = 6;
+		}
+		else if (e->getMapActuel() == 4) {
+			if (params[1] >= 17) 
+				params[1] = 16;
+			if (params[1] <= 4) 
+				params[1] = 5;
+			if (params[2] >= 14) 
+				params[2] = 13;
+			if (params[2] <= 2) 
+				params[1] = 3;
+		}
+
 		p.setEnDeplacement(true);
 		sf::Vector2f r = e->getCoordTile(params[1], params[2]);
 		#if TRACE_CO==1
