66728e9207771cc21585f97b02b3d91b41f12233
Romain Amuat <romain.amuat@gmail.com>
2016-11-24 17:33:18 +0100

    Correctif Dia

--- src/ia/Noeud.cpp
+++ src/ia/Noeud.cpp
@@ -1,6 +1,8 @@
 #include "../ia.h"
 #include "../state.h"
 
+#include <algorithm>
+
 using namespace state;
 using namespace ia;
 
@@ -26,3 +28,44 @@ state::Element * Noeud::getLien () {
 	return lien;
 }
 
+Noeud * Noeud::getPere() {
+	return pere;
+}
+
+state::Element * Noeud::MinMax (bool heros) {
+	if (fils.size() > 0) {
+		for (int i = 0; i < fils.size(); ++i) {
+			if (fils[i] != nullptr) {
+				(fils[i])->MinMax(heros);
+			}
+		}
+	}
+	else {
+		std::vector<Noeud *> ids;
+		Noeud * t = p;
+
+		while (t->getPere != nullptr) {
+			ids.push_back(t);
+			t = t->getPere();
+		}
+		
+		std::vector<int> morts;
+		for (int i = 0; i < ids.size(); ++i) {
+			std::vector <int> pos; 
+			for (int j = ids.size() - 1; j > 0 ; j--) {
+				if (ids[j].getID() == ids[i].getID())
+					pos.push_back(j);
+			}
+			if (pos.size() > 1) {
+				morts.push_back(i);
+				for (int j = 1; j < pos.size(); ++j) {
+					ids.erase(ids.begin() + pos[j]);
+				}
+			}
+		}
+
+		for (int i = 0; i < morts.size(); ++i) {
+			getTypeID
+		}
+	}
+}
